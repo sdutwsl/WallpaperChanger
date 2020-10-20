e2c573eb4a977dd2238c55668b65fdac0ed75191
sdutwsl <wanghuilongsd@163.com>
2020-10-20 17:36:57 +0800

    Add gnome script

--- Linux/WallpaperChanger.sh
+++ Linux/WallpaperChanger.sh
@@ -1,1 +1,23 @@
 #!/bin/bash
+
+doLoop(){
+		filetypes=("jpg" "jpeg")
+		files=""
+		for j in ${filetypes[@]}
+		do
+				files=$files" "`ls *|grep $j`
+		done
+		wd=`pwd`
+		for k in ${files[@]}
+		do
+				f="file:"$wd"/"$k
+				echo $f
+				gsettings set org.gnome.desktop.background picture-uri "'"$f"'"
+				sleep 10s
+		done
+}
+
+while true
+do
+	doLoop
+done
